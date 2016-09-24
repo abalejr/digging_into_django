66516fb53c0724327ef5a482f5bbdadcf8b13367
A.J. Bale <abalejr@gmail.com>
2016-09-23 20:27:34 -0500

    Add env, Project, and main_app

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\a.j\google_drive\programming\education\django\digging_into_django\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
