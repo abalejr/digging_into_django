7e527e8dd399884ffbf048459df4394d8d951ea9
A.J. Bale <abalejr@gmail.com>
2016-10-05 21:08:54 -0500

    Add Treasure model reference to main_app views

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from .models import Treasure
 
 # Create your views here.
 def index(request):
+	treasures = Treasure.objects.all()
 	return render(request, "index.html", {"treasures": treasures})
 
 
 """
 treasures = [
-	Treasure("Gold Nugget", 500.00, "gold", "Curly's Creek, NM", "http://staffordcountymuseum.com/wp-content/uploads/2014/02/gold-nugget-2.jpg"),
-	Treasure("Fool's Gold", 0, "pyrite", "Fool's Falls, CO", "http://www.gemstonebuzz.com/files/gemstone/pyrite-cube.jpg"),
-	Treasure("Coffee Can", 20.00, "tin", "Acme, CA", "http://italianfoodmaterialsandmachinery.com/files/2014/06/CoffeeCan_300dpi.png")
+	Treasure(name="Gold Nugget", value=500.00, material="gold", location="Curly's Creek, NM", img_url="http://staffordcountymuseum.com/wp-content/uploads/2014/02/gold-nugget-2.jpg"),
+	Treasure(name="Fool's Gold", value=0, material="pyrite", location="Fool's Falls, CO", img_url="http://www.gemstonebuzz.com/files/gemstone/pyrite-cube.jpg"),
+	Treasure(name="Coffee Can", value=20.00, material="tin", location="Acme, CA", img_url="http://italianfoodmaterialsandmachinery.com/files/2014/06/CoffeeCan_300dpi.png")
 ]		"""
