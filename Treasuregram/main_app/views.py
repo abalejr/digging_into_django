7f4294b320f9fa84d1c93b04f3fa01c6a5c6e88d
A.J. Bale <abalejr@gmail.com>
2016-10-08 12:21:20 -0500

    Add post_treasure view, add form to index view

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Treasure
+from .forms import TreasureForm
 
 # Create your views here.
 def index(request):
 	treasures = Treasure.objects.all()
-	return render(request, "index.html", {"treasures": treasures})
+	form = TreasureForm()
+	return render(request, "index.html", {"treasures": treasures, "form": form})
 
 def details(request, treasure_id):
 	treasure = Treasure.objects.get(id=treasure_id)
 	return render(request, "details.html", {"treasure": treasure})
 
-
-"""
-treasures = [
-	Treasure(name="Gold Nugget", value=500.00, material="gold", location="Curly's Creek, NM", img_url="http://staffordcountymuseum.com/wp-content/uploads/2014/02/gold-nugget-2.jpg"),
-	Treasure(name="Fool's Gold", value=0, material="pyrite", location="Fool's Falls, CO", img_url="http://www.gemstonebuzz.com/files/gemstone/pyrite-cube.jpg"),
-	Treasure(name="Coffee Can", value=20.00, material="tin", location="Acme, CA", img_url="http://italianfoodmaterialsandmachinery.com/files/2014/06/CoffeeCan_300dpi.png")
-]		"""
+def post_treasure(request):
+	form = TreasureForm(request.POST)
+	if form.is_valid():
+		treasure = Treasure(name = form.cleaned_data["name"],
+							value = form.cleaned_data["value"],
+							material = form.cleaned_data["material"],
+							location = form.cleaned_data["location"],
+							img_url = form.cleaned_data["img_url"])
+		treasure.save()
+	return HttpResponseRedirect("/")
